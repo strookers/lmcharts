2cc8ce81268d263d6fe45017e6220d9b6359e95f
strookers <michael_k@mail.dk>
2017-04-17 20:48:45 +0200

    added barchart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BarchartComponent } from './shared/barchart/barchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent
   ],
   imports: [
     BrowserModule,
